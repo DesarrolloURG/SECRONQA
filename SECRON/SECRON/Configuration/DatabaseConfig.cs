cede185de985fb59c8b4ed4faf388c6b3d081c95
DesarrolloURG <desarrollo@uregionalregion2.edu.gt>
2026-04-10 13:59:50 -0600

    Actualización Publicación QA
    Actualiación Publicación QA

--- SECRON/SECRON/Configuration/DatabaseConfig.cs
+++ SECRON/SECRON/Configuration/DatabaseConfig.cs
@@ -15,7 +15,7 @@ namespace SECRON.Configuration
         // Cadena de conexión centralizada BASE DE DATOS QA
         //private static readonly string connectionString = "Initial Catalog=SECRONQA; User ID=sa; Password=URdatabase24.; Data Source=172.16.0.153";
         // Cadena de conexión centralizada BASE DE DATOS DEVELOPMENT
-        private static readonly string connectionString = "Initial Catalog=SECRONDEV; User ID=sa; Password=URdatabase24.; Data Source=172.16.0.153";
+        private static readonly string connectionString = "Initial Catalog=SECRONQA; User ID=sa; Password=URdatabase24.; Data Source=172.16.0.153";
         // Cadena de conexión alternativa para entorno de desarrollo local
         //private static readonly string connectionString = "Initial Catalog=SECRON; User ID=sa; Password=URdatabase24.; Data Source=DESKTOP-G7KKLEL\\SQLEXPRESS";
         #endregion CadenaDeConexion

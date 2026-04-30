9fab4c1016b2053fcc035426200dbd6117a194a2
DesarrolloURG <desarrollo@uregionalregion2.edu.gt>
2026-04-30 13:55:30 -0600

    Actualización Versión 2.0.0.4
    Módulo de cuentas contables
    - Se incorporó la migración de la versión 1 a SECRON 2.0

--- SECRON/SECRON/Properties/AssemblyInfo.cs
+++ SECRON/SECRON/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Número de compilación
 //      Revisión
 //
-[assembly: AssemblyVersion("2.0.0.3")]
-[assembly: AssemblyFileVersion("2.0.0.3")]
+[assembly: AssemblyVersion("2.0.0.4")]
+[assembly: AssemblyFileVersion("2.0.0.4")]
